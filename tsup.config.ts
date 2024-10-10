a51de99283a9539be2ea22da87a81c3d6f55c78b
Alistair Stead <62936+alistairstead@users.noreply.github.com>
2024-10-10 14:44:54 +0100

    update repo and improve ci

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  bundle: false,
   clean: true,
   dts: true,
   entry: ["src/**/*.ts", "!src/**/*.test.*"],
   format: ["cjs", "esm"],
-  outDir: "lib",
+  outDir: "dist",
   sourcemap: true,
 });
